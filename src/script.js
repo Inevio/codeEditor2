b6c815e8d5c58efa0d6d7101b9c5f836dde581d2
Joaquin Araujo <joaquinaraujojs@gmail.com>
2018-01-19 16:34:51 -0400

    Se agrego una fuente que habia olvidado en Downloads (Flaticon.ttf)

---
+++ src/script.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+console.log('Hello World')
+
+// CodeMirror
+// var myCodeMirror = CodeMirror.fromTextArea($('#myTextArea')[0], {
+//   lineNumbers: true,
+//   mode: "javascript",
+//   theme: "monokai",
+//   keyMap: "sublime"
+// });
+
+// Sidebar
